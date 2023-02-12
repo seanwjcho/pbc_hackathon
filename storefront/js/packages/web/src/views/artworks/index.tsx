247b7834295510e1ae1e5f93f0f09e77fc339194
seanw <seanwjcho@gmail.com>
2023-02-12 03:51:43 -0500

    changed colors, added imgur link

--- storefront/js/packages/web/src/views/artworks/index.tsx
+++ storefront/js/packages/web/src/views/artworks/index.tsx
@@ -89,7 +89,7 @@ export const ArtworksView = () => {
                   tab={<span className="tab-title">Owned</span>}
                   key={ArtworkViewState.Owned}
                 >
-                  <img src="ohno.jpg" alt="a"></img>
+                  <img src="https://i.imgur.com/zE8dJWS.jpg" alt="a"></img>
                 </TabPane>
               )}
               {connected && (

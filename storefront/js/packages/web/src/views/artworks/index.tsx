2bba7c97e4c06eae780d36edeca5a902a8033f9e
seanw <seanwjcho@gmail.com>
2023-02-12 03:16:14 -0500

    added image to Owned pt2

--- storefront/js/packages/web/src/views/artworks/index.tsx
+++ storefront/js/packages/web/src/views/artworks/index.tsx
@@ -89,7 +89,7 @@ export const ArtworksView = () => {
                   tab={<span className="tab-title">Owned</span>}
                   key={ArtworkViewState.Owned}
                 >
-                  <img src="ohno.jpg">NFTid = 52, Collection = Syria Relief</img>
+                  <img src="ohno.jpg" alt="a">NFTid = 52, Collection = Syria Relief </img>
                 </TabPane>
               )}
               {connected && (

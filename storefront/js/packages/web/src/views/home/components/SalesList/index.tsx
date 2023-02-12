9c948e1d966309a0d8fdb8cc8ddf25c7791b895f
seanw <seanwjcho@gmail.com>
2023-02-12 04:26:29 -0500

    banner experiment  and new colors

--- storefront/js/packages/web/src/views/home/components/SalesList/index.tsx
+++ storefront/js/packages/web/src/views/home/components/SalesList/index.tsx
@@ -43,7 +43,7 @@ export const SalesListView = (props: { collectionMintFilter?: string }) => {
     <>
       {!props.collectionMintFilter && (
         <Banner
-          src="/main-banner.svg"
+          src="https://i.imgur.com/EHCfMX7.jpg"
           headingText="💖 Charity Chain 🔗"
           subHeadingText="Harnessing the power of the Solana Ecosystem to deliver relief
           globally."
